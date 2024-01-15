8e45032c363f69bd0ae95f606e59981b8af64dd3
Vishavjeet <vishavjeetsharma88@gmail.com>
2024-01-15 18:38:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { RelatedProducts } from '@algolia/recommend-react';
+import recommend from '@algolia/recommend';
 
-function App() {
+const recommendClient = recommend('RHJ7D2F98X', 'd24365333269f7b87040034c4570e85a');
+const indexName = 'small_index';
+
+
+
+const currentObjectID = 'f23bfc1b1b4ce_dashboard_generated_id'
+// (apartment, resident for sale)
+
+// const currentObjectID = 'bf852a7f6f438_dashboard_generated_id'
+// (townhouse, resident for sale)
+
+
+
+
+function RelatedItem({ item }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", gap: "0px 20px", paddingRight: "20px" }}>
+      <div>
+        <h3>{item?.name}</h3>
+        <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", gap: "0px 40px" }}>
+          <h4>{item?.['locationObj.full_name']}</h4>
+          <h4>|</h4>
+          <h4>{item?.offering_type}</h4>
+          <h4>|</h4>
+          <h4>{item.propertyType}</h4>
+          <h4>|</h4>
+          <h4>{item['sizeObj.value']} {item['sizeObj.unit']}</h4>
+        </div>
+        <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", gap: "0px 40px" }}>
+          {item?.amenity_names && JSON.parse(item?.amenity_names)?.map((i,k) => (
+            <h5 key={k}>{i}</h5>
+          ))
+          }
+        </div>
+        <p style={{ fontSize: 14 }}>{item.description}</p>
+        <h4>Price - {item['priceObj.value']} {item['priceObj.currency']}</h4>
+      </div>
+      <div >
+        <img src={item['brokerObj.logo']} />
+      </div>
     </div>
   );
 }
 
-export default App;
+
+function App() {
+  return (
+    <>
+    <RelatedProducts
+      recommendClient={recommendClient}
+      indexName={indexName}
+      objectIDs={[currentObjectID]}
+      itemComponent={RelatedItem}
+    />
+    </>
+  );
+}
+
+
+export default App
